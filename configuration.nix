965328711e140f912896806eef74c20744a62461
karlowiczpl <karol.kar1@outlook.com>
2026-05-25 18:20:33 +0200

    clean up the configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -6,37 +6,22 @@
     ./desktop.nix
   ];
 
-
-  programs.fish.enable = true;
-
   systemd.tmpfiles.rules = [
     "Z /etc/nixos 0755 karol users -"
   ];
 
-  programs.neovim.enable = true;
-
-  # boot.loader.systemd-boot.enable = true;
   boot.loader.limine.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   nixpkgs.config.allowUnfree = true;
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "karolpc";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "pl_PL.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -71,23 +56,10 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
     ];
-    shell = pkgs.fish;
+    shell = pkgs.bash;
   };
-
-  # Allow unfree packages
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  system.stateVersion = "25.11"; # Did you read the comment?
-
-# W pliku /etc/nixos/configuration.nix
-
-	virtualisation.libvirtd.enable = true;
-	programs.virt-manager.enable = true; # Opcjonalne, ale pomaga w debugowaniu
-
-	users.users.TWOJA_NAZWA_UZYTKOWNIKA = {
-	  isNormalUser = true;
-	  extraGroups = [ "wheel" "libvirtd" "kvm" ]; # Dodaj kvm i libvirtd
-	};
-
+  system.stateVersion = "25.11";
 }
